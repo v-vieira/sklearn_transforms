0e5d05733a1224f0cd90226ebc1d67b409ab1ca4
v-vieira <54221945+v-vieira@users.noreply.github.com>
2020-08-23 21:04:00 -0300

    Update pacotes

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -15,6 +15,20 @@ class DropColumns(BaseEstimator, TransformerMixin):
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
 
+# Substituir as notas que são maior que 10 por 10
+class Subs_zeros_lim10():
+    def __init__(self,columns_norm):
+        self.columns_norm = columns_norm
+    def replace(self, input_dataframe):
+        #copia o dataframe
+        data = input_dataframe.copy()
+        #substitui
+        for col in self.columns_norm:
+            mean_no_zeros = float(data.loc[data[col] != 0,[col]].min())
+            data[col] = data[col].replace(0,mean_no_zeros)
+        data['NOTA_GO'].values[data['NOTA_GO'] > 10] = 10
+        return data
+    
 # Criação das colunas de média em humanas e média em exatas
 class medias():
     def create(self, input_dataframe):
